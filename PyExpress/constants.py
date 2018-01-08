c20681923e33d424d6f02dd05b50b3b67a01e06b
iahvector <iah.vector@gmail.com>
2018-01-08 22:30:29 +0200

    Rename HEADER constant

--- PyExpress/constants.py
+++ PyExpress/constants.py
@@ -3,6 +3,6 @@ This module collects the constants that can are useful but don't fall under a
 specific part of the code
 """
 
-CONTENT_TYPE_HEADER = 'Content-Type'
+HEADER_CONTENT_TYPE = 'Content-Type'
 CONTENT_TYPE_JSON = 'application/json'
 CONTENT_TYPE_HTML = 'text/html'
